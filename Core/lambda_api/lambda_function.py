6c3296517c215fddc2687760b46530422eb8bd31
Aliaksei Stralkou <aliaksei.stralkou@innowise-group.com>
2022-07-18 10:55:50 +0300

    refactor: methods and variables
    API methods are added in a separate file.
    Remove enums - variables moved to function scope.

---
+++ Core/lambda_api/lambda_function.py
@@ -0,0 +1,62 @@
+import json
+import logging
+
+import boto3
+
+from methods import (get_home,
+                     get_customer,
+                     post_customer,
+                     patch_customer,
+                     delete_customer,
+                     get_customers,
+                     build_response)
+
+
+def lambda_handler(event, context):
+    '''Implemention of CRUD methods.'''
+
+    TABLE_NAME = "MallCustomers"
+
+    GET = "GET"
+    POST = "POST"
+    PATCH = "PATCH"
+    DELETE = "DELETE"
+
+    HOME = "/home"
+    CUSTOMER = "/customer"
+    CUSTOMERS = "/customers"
+
+    table = boto3.resource("dynamodb").Table(TABLE_NAME)
+    logger = logging.getLogger()
+
+    http_method = event["httpMethod"]
+    path = event["path"]
+
+    logger.setLevel(logging.INFO)
+    logger.info(event)
+
+    if http_method == GET and path == HOME:  # GET to /home
+        response = get_home()
+
+    elif http_method == GET and path == CUSTOMER:  # GET to /customer
+        response = get_customer(table, logger, event["queryStringParameters"]["id"])
+
+    elif http_method == POST and path == CUSTOMER:  # POST to /customer
+        response = post_customer(table, logger, json.loads(event["body"]))
+
+    elif http_method == PATCH and path == CUSTOMER:  # PATCH to /customer
+        request_body = json.loads(event["body"])
+        response = patch_customer(table, logger,
+                                  request_body["id"], request_body["update_key"], request_body["update_value"])
+
+    elif http_method == DELETE and path == CUSTOMER:  # DELETE to /customer
+        request_body = json.loads(event["body"])
+        response = delete_customer(table, logger, request_body["id"])
+
+    elif http_method == GET and path == CUSTOMERS:  # GET to /customers
+        response = get_customers(table, logger)
+
+    else:
+        response = build_response(404, "Not Found :'(")
+
+    return response
---
+++ Core/lambda_api/methods.py
@@ -0,0 +1,138 @@
+import json
+
+from encoder import CustomEncoder
+
+
+def build_response(status_code, body=None):
+    '''Create responses function.'''
+
+    response = {
+        "statusCode": status_code,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        }
+    }
+
+    if body is not None:
+        response["body"] = json.dumps(body, cls=CustomEncoder)
+
+    return response
+
+
+def get_home():
+    '''GET - http://.../home'''
+
+    return build_response(200, "Hello from home page! :D")
+
+
+def get_customer(table, logger, id):
+    '''GET - http://.../customer'''
+
+    try:
+        response = table.get_item(
+            Key={
+                "id": id
+            }
+        )
+
+    except:
+        logger.exception("Do your custom error here. I am just gonna log it out here!")
+
+    if "Item" in response:
+        return build_response(200, response["Item"])
+
+    else:
+        return build_response(404, {"Message": f"Customer with ({id}) id not found."})
+
+
+def post_customer(table, logger, request_body):
+    '''POST - http://.../customer'''
+
+    try:
+        table.put_item(Item=request_body)
+        body = {
+            "Operation": "SAVE",
+            "Message": "SUCCESS",
+            "Item": request_body
+        }
+
+    except:
+        logger.exception("Do your custom error here. I am just gonna log it out here!")
+
+    return build_response(200, body)
+
+
+def patch_customer(table, logger, id, update_key, update_value):
+    '''PATCH - http://.../customer'''
+
+    try:
+        response = table.update_item(
+            Key={
+                "id": id
+            },
+            UpdateExpression="set %s = :value" % update_key,
+            ExpressionAttributeValues={
+                ":value": update_value
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+
+        body = {
+            "Operation": "UPDATE",
+            "Message": "SUCCESS",
+            "UpdateAttrebutes": response
+        }
+
+    except:
+        logger.exception("Do your custom error here. I am just gonna log it out here!")
+
+    return build_response(200, body)
+
+
+def delete_customer(table, logger, id):
+    '''DELETE - http://.../customer'''
+
+    try:
+        respone = table.delete_item(
+            Key={
+                "id": id
+            },
+            ReturnValues="ALL_OLD"
+        )
+
+        body = {
+            "Operation": "DELETE",
+            "Message": "SUCCESS",
+            "deletedItem": respone
+        }
+
+    except:
+        logger.exception("Do your custom error here. I am just gonna log it out here!")
+
+    if "Attributes" in body["deletedItem"]:
+        return build_response(200, body)
+
+    else:
+        return build_response(404, {"Message": f"Customer with ({id}) id not found."})
+
+
+def get_customers(table, logger):
+    '''GET - http://.../customers'''
+
+    try:
+        response = table.scan()
+        result = response["Items"]
+
+        while "LastEvaluatedKey" in response:
+            response = table.scan(ExclusiveStartKey=response["LastEvaluatedKey"])
+            result.extend(response["Items"])
+
+        body = {
+            "products": result
+        }
+
+    except:
+        logger.exception("Do your custom error here. I am just gonna log it out here!")
+
+    return build_response(200, body)
